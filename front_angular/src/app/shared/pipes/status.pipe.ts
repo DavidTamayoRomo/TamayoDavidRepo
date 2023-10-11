2c2cd907a1e9b3c3192e40d62077e4b793d33b55
dtamayo <franklin.tamayo@quito.gob.ec>
2023-10-11 16:14:35 -0500

    ft - pipe front

--- front_angular/src/app/shared/pipes/status.pipe.ts
+++ front_angular/src/app/shared/pipes/status.pipe.ts
@@ -5,8 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StatusPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: unknown, ...args: unknown[]): string  {
+    if (value == 'verify') {
+      return 'VERIFICADO';
+    }else if (value == 'approved') {
+      return 'APROBADO';
+    }else if (value == 'rejected') {
+      return 'RECHAZADO';
+    }
+    return 'PENDIENTE';
   }
 
 }
